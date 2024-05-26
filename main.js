abc0cc6ebfd5396ab19e6a436a325f627689a921
gkelshelashvili <giorgikesho6@gmail.com>
2024-05-26 11:37:10 +0400

    done

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
 const replaceString = (string,valueToReplace,valueToReplaceWith) => {
+    let result = '';
+    let first = 0;
+    let last = 0;
 
+    if((first = string.indexOf(valueToReplace, last)) !== -1) {
+        result += string.substring(last, first) + valueToReplaceWith;
+        last = first + valueToReplace.length;
+    }
+    result += string.substring(last);
+
+    return result;
 }
 
-const replaceWithUpperCase = (sentance) => {
+console.log(replaceString('gela', 'g', 'T'))
 
+const replaceWithUpperCase = (sentance) => {    
+
+    const words = sentance.split(' ')
+
+    let wordsUpdated = words.map(function (maped_words) {
+        return maped_words.charAt(0).toUpperCase() + maped_words.slice(1).toLowerCase()
+    })
+
+
+    return wordsUpdated.join(' ')
+    
 }
+console.log(replaceWithUpperCase('naxes ucxo moyme vinme'))
 
 const users = [
     {name: "Lasha", age: 30},
